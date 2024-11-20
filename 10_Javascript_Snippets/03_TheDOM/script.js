b6cec44fb76ececeb362bd70f1dd1409b63e9115
Corinne Dascenzo <coco6612@gmail.com>
2024-11-20 15:05:27 +0100

    Add samples

---
+++ 10_Javascript_Snippets/03_TheDOM/script.js
@@ -0,0 +1,9 @@
+document.querySelector("h1.ranking").innerHTML =
+  "<p>Most popular Harry Potter characters</p>";
+
+document.getElementById("fourth").innerHTML = "Professor Snape";
+
+document.getElementsByClassName("slytherin")[1].innerHTML =
+  "Salazar Slytherinnnn";
+
+document.getElementsByTagName("li")[0].innerHTML = "Dobby";
---
+++ 10_Javascript_Snippets/04_StyleAnElement/script.js
@@ -0,0 +1,4 @@
+let blueElement = document.getElementById("body");
+blueElement.style.backgroundColor = "#000000";
+
+document.querySelector(".heading").style.fontFamily = "Roboto";
